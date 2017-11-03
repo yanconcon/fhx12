d444099c7c9e11708e9dab66ffe384ecb1f4a23b
lenovo <1029878802@qq.com>
2017-11-03 21:05:15 +0800

    2017/11/3

---
+++ blog/static/langs/zh_cn.js
@@ -0,0 +1,54 @@
+/*
+* writed by xiaoyan in 2017*11/3
+* 谷歌翻译中文版
+*
+*
+* */
+
+$.trumbowyg.langs.zh_cn = {
+    viewHTML:       "源代码格式",
+
+    formatting:     "输入格式",
+    p:              "段落",
+    blockquote:     "块引用",
+    code:           "代码",
+    header:         "开头",
+
+    bold:           "苍劲",
+    italic:         "斜体",
+    strikethrough:  "删除线",
+    underline:      "下划线",
+
+    strong:         "着重",
+    em:             "重点",
+    del:            "删除符",
+
+    unorderedList:  "无序排列",
+    orderedList:    "有序排列",
+
+    insertImage:    "插入图片",
+    insertVideo:    "插入视频",
+    link:           "链接",
+    createLink:     "创建链接",
+    unlink:         "取消链接",
+
+    justifyLeft:    "左对齐",
+    justifyCenter:  "中间对其",
+    justifyRight:   "右对齐",
+    justifyFull:    "对齐",
+
+    horizontalRule: "Vaakaviiva",
+
+    fullscreen:     "全屏",
+
+    close:          "关闭",
+
+    submit:         "提交",
+    reset:          "重置",
+
+    invalidUrl:     "地址无效",
+    required:       "请求",
+    description:    "描述",
+    title:          "标题",
+    text:           "内容"
+}
