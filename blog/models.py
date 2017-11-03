c5ad49fdeed674f81f495269c8a4eebfcc46749b
玩我鸟 <xuakngkoon@foxmail.com>
2017-11-03 21:17:08 +0800

    2017/11/3

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,25 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
+class MyUser(AbstractUser):
+	jifen = models.IntegerField('积分', default=0)
+
+	class Meta:
+		db_table = 'MyUser'
+
+	def __str__(self):
+		return self.username
+
+
+class UserLogin(models.Model):
+	username = models.CharField(max_length=15)
+	password = models.CharField(max_length=50)
+
+
 class Article(models.Model):
 	title=models.CharField("标题",max_length=50)
-	zhuozhe=models.CharField("作者",max_length=50)
+	authors=models.ManyToManyField(MyUser)
 	created_date=models.DateField("创建日期",auto_now_add=True)
 	modify_date=models.DateField("修改日期",auto_now=True)
 	content=models.TextField()	
@@ -18,17 +34,5 @@ class Article(models.Model):
 	def __str__(self):
 		return self.title
 
-class MyUser(AbstractUser):
-	
-	jifen=models.IntegerField('积分',default=0)
-
-	class Meta:
-		db_table='MyUser'
-	def __str__(self):
-		return self.username
-		
-class UserLogin(models.Model):
-	username = models.CharField(max_length=15)
-	password = models.CharField(max_length=50)
 
 
